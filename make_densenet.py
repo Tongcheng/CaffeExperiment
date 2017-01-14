eb38b4594106af21b490321b3becb6558ae4fe42
Tongcheng Li <tl486@cornell.edu>
2017-01-14 23:22:55 +0800

    add dropout backin

--- make_densenet.py
+++ make_densenet.py
@@ -9,23 +9,21 @@ def bn_relu_conv(bottom, ks, nout, stride, pad, dropout):
     relu = L.ReLU(scale, in_place=True,negative_slope=0.5)
     conv = L.Convolution(relu, kernel_size=ks, stride=stride, 
                     num_output=nout, pad=pad, bias_term=False, weight_filler=dict(type='msra'), bias_filler=dict(type='constant'))
-    #if dropout>0:
-    #    conv = L.Dropout(conv, dropout_ratio=dropout)
+    if dropout>0:
+        conv = L.Dropout(conv, dropout_ratio=dropout)
     return conv
 
 def conv_bn_relu(bottom, ks, nout, stride, pad, dropout):
     conv = L.Convolution(bottom, kernel_size=ks, stride=stride, 
                     num_output=nout, pad=pad, bias_term=False, weight_filler=dict(type='msra'), bias_filler=dict(type='constant'))
-    #if dropout>0:
-    #    conv = L.Dropout(conv, dropout_ratio=dropout)
+    if dropout>0:
+        conv = L.Dropout(conv, dropout_ratio=dropout)
     batch_norm = L.BatchNorm(conv, in_place=False, param=[dict(lr_mult=0, decay_mult=0), dict(lr_mult=0, decay_mult=0), dict(lr_mult=0, decay_mult=0)])
     scale = L.Scale(batch_norm, bias_term=True, in_place=True, filler=dict(value=1), bias_filler=dict(value=0))
     relu = L.ReLU(scale, in_place=True,negative_slope=0.5)
     return relu
 
 def add_layer(bottom, num_filter, dropout):
-    #conv = bn_relu_conv(bottom, ks=3, nout=num_filter, stride=1, pad=1, dropout=dropout)
-    #concate = L.Concat(bottom, conv, axis=1)
     newC = conv_bn_relu(bottom, ks=3, nout=num_filter, stride=1, pad=1, dropout=dropout)
     concate = L.Concat(bottom, newC, axis=1)
     return concate
@@ -63,10 +61,6 @@ def densenet(data_file, mode='train', batch_size=64, depth=40, first_output=16, 
         model = add_layer(model, growth_rate, dropout)
         nchannels += growth_rate
 
-
-    #model = L.BatchNorm(model, in_place=False, param=[dict(lr_mult=0, decay_mult=0), dict(lr_mult=0, decay_mult=0), dict(lr_mult=0, decay_mult=0)])
-    #model = L.Scale(model, bias_term=True, in_place=True, filler=dict(value=1), bias_filler=dict(value=0))
-    #model = L.ReLU(model, in_place=True)
     model = L.Pooling(model, pool=P.Pooling.AVE, global_pooling=True)
     model = L.InnerProduct(model, num_output=10, bias_term=True, weight_filler=dict(type='xavier'), bias_filler=dict(type='constant'))
     loss = L.SoftmaxWithLoss(model, label)
