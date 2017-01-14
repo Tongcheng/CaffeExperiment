06785edc9351ffe8df69855196ba97dae192c21c
Tongcheng Li <tl486@nikola-compute01.coecis.cornell.edu>
2017-01-14 09:56:42 -0500

    new

--- train.sh
+++ train.sh
@@ -1,6 +1,7 @@
 set -e
 
 TOOLS=/home/tl486/git_TCcaffe/caffe2/caffe/build/tools
+#TOOLS=$CAFFE_ROOT/build/tools
 
 LD_PRELOAD="/opt/OpenBLAS/lib/libopenblas.so.0" $TOOLS/caffe train \
-  --solver=solver.prototxt
+  --solver=solver.prototxt 
